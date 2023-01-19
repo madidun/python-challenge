a44aedf6e985b5c8a7c2c69e919f5821ec5cc9ea
madidun <119899172+madidun@users.noreply.github.com>
2023-01-18 23:34:25 -0500

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,14 @@
 import os
 import csv
 
+total_profit = 0
+candidates = []
+total_votes = []
+total_months = 0
+total_profit_losses = 0
+profit_loss_change = 0
+profit_increase = 0
+
 budget_data=os.path.join = "PyPoll/Resources/budget_data.csv"
 
 with open(budget_data,"") as csv
