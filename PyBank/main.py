6623c05aea9e8540644f2dcdc5691f7c6b5ad0b4
madidun <119899172+madidun@users.noreply.github.com>
2023-01-18 23:33:10 -0500

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,15 @@
+import os
+import csv
 
+budget_data=os.path.join = "PyPoll/Resources/budget_data.csv"
+
+with open(budget_data,"") as csv
+csvreader =csvreader(csvfile,delimiter="" ) 
+csv_header = next(csvreader)
+
+for row in csv_reader
+
+total vote +=1
+
+print(financial analysis)
+print("-------------------")
